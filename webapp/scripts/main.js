3511a96c6371647237725c500d560082f7d3e57e
Tobias Lundin <tobias.lundin@gmail.com>
2012-10-23 21:09:00 +0200

    updated user features

--- webapp/scripts/main.js
+++ webapp/scripts/main.js
@@ -1,9 +1,9 @@
 
 require(["jquery", "cmd", "modules/output", "modules/user"], function($, Cmd, Output, User) {
 
-	window.user = User;
-	User.init('Tobias', 'Oslo');
+	User.init(name, location);
 	var output = new Output($("#output"));
 	var cmd = new Cmd($("#console"), output);
 	cmd.seyHello();
+	
 });
