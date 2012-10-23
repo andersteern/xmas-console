209821ca94c32340b7d353d3bf5e9c301f0dba34
Tobias Lundin <tobias.lundin@gmail.com>
2012-10-23 20:26:57 +0200

    pubnub fix

--- webapp/scripts/main.js
+++ webapp/scripts/main.js
@@ -1,9 +1,9 @@
 
 require(["jquery", "cmd", "modules/output", "modules/user"], function($, Cmd, Output, User) {
 
-	window.user = User.init('Tobias', 'Oslo');
+	window.user = User;
+	User.init('Tobias', 'Oslo');
 	var output = new Output($("#output"));
 	var cmd = new Cmd($("#console"), output);
 	cmd.seyHello();
-
 });
