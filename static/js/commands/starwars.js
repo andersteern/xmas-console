f9ac6aa369fc96475c738be6f3224fc483fabfe3
Christoffer Winter-Hjelm <chriwint@gmail.com>
2012-12-08 23:06:04 +0100

    Added possibility to cancel starwars by writing starwars.

--- static/js/commands/starwars.js
+++ static/js/commands/starwars.js
@@ -4,13 +4,20 @@ define(["modules/output"], function(output){
 	};
 	Command.prototype.run = function(args) {
 		var outputElement = output.getElement();
+
+		if(outputElement.hasClass("starwars")) {
+			outputElement.removeClass("starwars");
+			output.print("The force is no longer with you");
+		} else {
+			outputElement.addClass("starwars").removeClass("spin");
+			setTimeout(function() {
+				outputElement.addClass("spin");
+			}, 3000);
+			
+			output.print("May the force be with you");
+		}
 		
-		outputElement.addClass("starwars").removeClass("spin");
-		setTimeout(function() {
-			outputElement.addClass("spin");
-		}, 5000);
 		
-		output.print("May the force be with you");
 	};
 
 	return Command;
