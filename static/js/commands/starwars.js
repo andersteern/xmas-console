827633781f6033944966aa0d171cf1820d31f335
Christoffer Winter-Hjelm <chriwint@gmail.com>
2012-12-09 00:30:10 +0100

    Changed so that starwars can be stopped with "starwars -r" instead.

--- static/js/commands/starwars.js
+++ static/js/commands/starwars.js
@@ -5,16 +5,19 @@ define(["modules/output"], function(output){
 	Command.prototype.run = function(args) {
 		var outputElement = output.getElement();
 
-		if(outputElement.hasClass("starwars")) {
-			outputElement.removeClass("starwars");
-			output.print("The force is no longer with you");
-		} else {
-			outputElement.addClass("starwars").removeClass("spin");
-			setTimeout(function() {
-				outputElement.addClass("spin");
-			}, 3000);
-			
-			output.print("May the force be with you");
+		switch (args[0]) {
+            case "-r":
+                outputElement.removeClass("starwars");
+				output.print("The force is no longer with you");
+                break;
+
+            default:
+            	outputElement.addClass("starwars").removeClass("spin");
+				setTimeout(function() {
+					outputElement.addClass("spin");
+				}, 3000);
+				
+				output.print("May the force be with you");
 		}
 		
 		
