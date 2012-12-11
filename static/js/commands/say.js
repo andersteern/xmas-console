8ace3e6f877c3590ddb998b87f5fea646d83947c
Tobias Lundin <tobias.lundin@gmail.com>
2012-12-11 19:49:21 +0100

    fix med msg till chat

--- static/js/commands/say.js
+++ static/js/commands/say.js
@@ -6,13 +6,14 @@ define(
 	};
 	Command.prototype.run = function(args) {
 		var msg = args.join(" ");
-		if(msg.length === 0){
+		if(msg.trim().length === 0){
 			output.print('  - "say" &lt;message&gt; sends to everyone');
 			output.print('  - "say" @&lt;username&gt; &lt;message&gt; to only send to &lt;username&gt;');
 		} else {
 			var uname = args.shift(), usrname;
 			if(uname.charAt(0) === '@'){
 				usrname = uname.substr(1);
+				msg = args.join(' ');
 			}
 			output.print(cookie.getName() + ": \"" + msg + "\"");
 			chat.send(encodeURIComponent(msg), usrname);
