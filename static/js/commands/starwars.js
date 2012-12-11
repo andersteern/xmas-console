6f487f7fd677a2578e9033b03630ba328a04e1c4
Tobias Lundin <tobias.lundin@gmail.com>
2012-12-11 19:11:43 +0100

    filade till starwars command med musik

--- static/js/commands/starwars.js
+++ static/js/commands/starwars.js
@@ -1,23 +1,38 @@
-define(["modules/output"], function(output){
+define(["modules/output", "modules/player"], function(output, player){
+
+	var outputElement, myPlayer;
 
 	var Command = function() {
+		outputElement = output.getElement();
+		myPlayer = new player('https://dl.dropbox.com/u/4512000/star_wars_intro.mp3');
 	};
+
+	function toggleStarwarsMode () {
+		// body...
+		outputElement.addClass("starwars");
+		outputElement.toggleClass("spin");
+
+		var init = outputElement.hasClass('spin');
+		var message = init ? "May the force be with you" : "The force is no longer with you" ;
+		setTimeout(function(){
+			output.print(message);
+			if(init){
+				myPlayer.play();
+			} else {
+				myPlayer.pause();
+			}
+		}, 2100);
+	}
+
 	Command.prototype.run = function(args) {
-		var outputElement = output.getElement();
 
 		switch (args[0]) {
-            case "-r":
-                outputElement.removeClass("starwars");
-				output.print("The force is no longer with you");
-                break;
+			case "-h":
+				output.print('just do it man!');
+				break;
 
-            default:
-            	outputElement.addClass("starwars").removeClass("spin");
-				setTimeout(function() {
-					outputElement.addClass("spin");
-				}, 3000);
-				
-				output.print("May the force be with you");
+			default:
+				toggleStarwarsMode();
 		}
 		
 		
