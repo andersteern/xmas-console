3a29c8fd897b9243b094e95194c14e331eccddad
Tobias Lundin <tobias.lundin@gmail.com>
2012-12-12 16:26:28 +0100

    added access granted and denied via keyboard

--- static/js/commands/fake.js
+++ static/js/commands/fake.js
@@ -7,6 +7,7 @@ define(["modules/output", "modules/player"], function(output, Player){
 			currentLine: null,
 			interval: null,
 			state: 'off',
+			fullscreenEl: null,
 			outputEl: null,
 			dataUrl: 'https://dl.dropbox.com/u/4512000/fakeWorkData.txt',
 			soundUrl: 'https://dl.dropbox.com/u/4512000/TypingSound.mp3'
@@ -14,8 +15,50 @@ define(["modules/output", "modules/player"], function(output, Player){
 		elkSoundUrl = 'https://dl.dropbox.com/u/4512000/elk-orgasm.mp3',
 		fakeLaughUrl = 'https://dl.dropbox.com/u/4512000/fakeLaugh.mp3';
 
+	function denyOrGrantAccess (bool) {
+		if($('.grant').length === 0){
+			var fullscreenEl = output.getFullscreenForegroundElement();
+			fullscreenEl.append('<h2 class="access deny">ACCESS DENIED</h2>');
+			fullscreenEl.append('<h2 class="access grant">ACCESS GRANTED</h2>');
+			$('.access').css({
+				'display' : 'none',
+				'text-align' : 'center',
+				'margin' : 'auto',
+				'border' : '5px solid lime',
+				'background-color' : 'black',
+				'width' : '470px',
+				'height' : '60px',
+				'font-size' : '50px',
+				'position' : 'fixed',
+				'left' : '0',
+				'top' : '0',
+				'right' : '0',
+				'bottom' : '0',
+				'padding-top': '10px'
+			});
+		}
+		if(bool){
+			$('.deny').hide();
+			$('.grant').toggle();
+		} else {
+			$('.grant').hide();
+			$('.deny').toggle();
+		}
+	}
+
+	function handleKeyboardInput (e) {
+		if(e.altKey){
+			if(e.keyCode === 83){ //s
+				denyOrGrantAccess(false);
+			} else if(e.keyCode === 65){ //a
+				denyOrGrantAccess(true);
+			}
+		}
+	}
 
 	function startFakeWork () {
+		$('body').on('keydown', handleKeyboardInput);
+
 		fakeWorkConf.outputEl = output.getPrintElement();
 		fakeWorkConf.dataClone = fakeWorkConf.data.slice(0);
 		fakeWorkConf.currentLine = fakeWorkConf.dataClone.shift().match(/.{1,3}/g);
@@ -57,6 +100,7 @@ define(["modules/output", "modules/player"], function(output, Player){
 			player.pause();
 			output.print(' aaaaand its coffee time!!!');
 			fakeWorkConf.state = 'off';
+			$('body').off('keydown', handleKeyboardInput);
 		} else {
 			output.print(' cant STOP work that is not in progress!');
 		}
@@ -109,6 +153,7 @@ define(["modules/output", "modules/player"], function(output, Player){
 
 			output.print('  - Usage: fake { work | laugh | elk-orgasm }');
 			output.print('  - control fake work via: fake { work-pause | work-resume | work-stop }');
+			output.print('  - extra hacker skill provided by: alt + a, alt + s');
 
 		} else {
 			var arg = args.shift().toLowerCase();
